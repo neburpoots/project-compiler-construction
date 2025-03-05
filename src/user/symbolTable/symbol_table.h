a3a99e74f4825575f05916347730ec35d15f45d3
Nick Glas <nickglas@hotmail.nl>
2025-03-05 12:34:04 +0100

    Added custom user struct in traversal

---
+++ src/user/symbolTable/symbol_table.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <stdbool.h>
+#include <stdlib.h>
+
+typedef struct stable stable_st;
+
+stable_st *STnew(size_t size);
+
--- src/user_types.h
+++ src/user_types.h
@@ -1,6 +1,9 @@
 #pragma once
 
 #include "palm/hash_table.h"
+#include "user/symbolTable/symbol_table.h"
 
 typedef htable_st* htable_stptr;
+
 // Add more types here if necessary
+typedef stable_st* stable_stptr;
